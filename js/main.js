97846b58460908d86f6ea39c5fd4da6beee37da6
Juberlandio <juberlandio.santana.js@gmail.com>
2023-11-24 16:13:26 -0300

    anime

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Ativar link da pagina
-const links = document.querySelectorAll("#menu a");
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
-  if (url.includes(href)) {
-    console.log(link);
-    link.classList.add("active");
-  }
-}
-links.forEach(ativarLink);
-
 // Ativar botão menu hamburger
 const btnMenu = document.querySelector("#btn-mobile");
 function toggleMenu() {
@@ -23,6 +11,7 @@ if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
+// Menu
 function fixarMenu(){
   let header = document.querySelector('#header')
   header.classList.toggle('fixar', window.scrollY > 0)
