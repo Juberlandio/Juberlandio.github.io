e5c16b12a1d1feafb22e6b9500c1790a9607e3c8
Juberlandio <juberlandio.santana.js@gmail.com>
2023-12-04 21:09:46 -0300

    habilidades

--- js/habilidades.js
+++ js/habilidades.js
@@ -16,7 +16,7 @@ const sobreSkill = [
 
                     '<p style="font-weight: bold; display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">Git</p> <br> <p style="display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">Sistema de controle de versão distribuído amplamente utilizado para o rastreamento de alterações em arquivos de código-fonte. </p> <br>',
 
-                    '<p style="font-weight: bold; display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">Github</p> <br> <p style="display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+                    '<p style="font-weight: bold; display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">Github</p> <br> <p style="display: block; animation: opacity 3s forwards; @keyframes mostrar {from {opacity: 0;transform: matrix3d(-30px, 0, 0);}to {opacity: 1;transform: matrix3d(0, 0, 0);}}">É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>',                       
 ]
 const dataInicio = [
                     (new Date(2021, 10, 1)).getTime(), // 0
@@ -26,7 +26,10 @@ const dataInicio = [
                     (new Date(2022, 3, 1)).getTime(), // 4
                     (new Date(2022, 1, 1)).getTime(), // 5
                     (new Date(2021, 11, 1)).getTime(), // 6
-                    (new Date(2021, 11, 1)).getTime(), // 7
+                    (new Date(2023, 11, 1)).getTime(), // 7
+                    (new Date(2023, 11, 1)).getTime(), // 8
+                    (new Date(2023, 11, 1)).getTime(), // 9
+                    (new Date(2023, 11, 1)).getTime(), // 10
                     ]
 const dataHoje = (new Date()).getTime();
 
@@ -37,7 +40,7 @@ skills.forEach(  (elemento, index) => {
         let tempo = 'mês';
         let tempoDeExperiencia = Math.round((dataHoje-dataInicio[index1])/(1000*60*60*24*30));
         if (tempoDeExperiencia > 1) tempo = 'meses'
-        descricao.innerHTML = `<p>${sobreSkill[index1]} </p>` ;
+        descricao.innerHTML = `<p>${sobreSkill[index1]}</p>` ;
     } )
     elemento.addEventListener('mouseout', (evento, elemento,) => {
         descricao.innerHTML = '/* Passe o mouse por cima de alguma habilidade para ler a descrição */';
