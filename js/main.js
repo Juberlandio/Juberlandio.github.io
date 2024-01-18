b9032e8fa2b776686561c8a172685436af8b0b01
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-01-18 08:15:05 -0300

    Commit Inicial

--- js/main.js
+++ js/main.js
@@ -75,3 +75,24 @@ function initAnimacaoScroll() {
   }
 }
 initAnimacaoScroll();
+
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion');
+  const activeClass = 'ativo';
+  
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
+initAccordion();
