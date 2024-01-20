8f5af16f0eb09c0d96b0d23eb3f07eb4626f9c65
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-01-20 14:38:23 -0300

    Commit Inicial

--- js/main.js
+++ js/main.js
@@ -75,3 +75,16 @@ function initAnimacaoScroll() {
   }
 }
 initAnimacaoScroll();
+
+function animacaoButton() {
+const button = document.getElementById('read_button');
+
+button.addEventListener('click', function() {
+  const card = document.querySelector('.card');
+  card.classList.toggle('active');
+
+  if (card.classList.contains('active')) {
+  }
+});
+}
+animacaoButton();
