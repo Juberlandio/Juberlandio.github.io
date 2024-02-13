4836c91495362ec770130f405a42bcfc9f6d55cc
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-02-13 18:03:31 -0300

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click'];
+  
+  if(menuButton) {
+  function openMenu(event) {
+    menuList.classList.add('active');
+    menuButton.classList.add('active');
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active');
+      menuButton.classList.remove('active');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
+}
+initMenuMobile()
+
+ function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+
+  if(!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
+    element.setAttribute(outside, '');
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      })
+      callback();
+    }
+  }
+}
+outsideClick();
