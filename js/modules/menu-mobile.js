13831920f9b74aecf7ec82cc2644063ad5edb695
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-03-10 17:02:59 -0300

    Update click

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,7 +3,7 @@ import outsideClick from './outsideclick.js';
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ['click', 'touchstart'];
+  const eventos = ['click'];
 
   function openMenu() {
     menuList.classList.add('active');
