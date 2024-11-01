cf9dbfc91435e6fcbd4c28b9a8fa11c492e7e2ca
juberlandio <juberlandio.santana.js@gmail.com>
2024-11-01 15:08:42 -0300

    Update maquina de escrever

--- js/modules/maquina-escrever.js
+++ js/modules/maquina-escrever.js
@@ -1,13 +1,22 @@
-export default function initMaquinaEscrever(){
+export default function initMaquinaEscrever() {
+    let repeatCount = 0; // Inicializa o contador
+    const maxRepeats = 5; // Define o número máximo de repetições
+
     function typeWriter(elemento) {
         const textoArray = elemento.innerHTML.split('');
         elemento.innerHTML = '';
         setTimeout(() => {
-        for(let i = 0; i < textoArray.length; i++) {
-            setTimeout(() => elemento.innerHTML += textoArray[i], 170 * i);
-        }}, 1500);
-        setTimeout(() => typeWriter(titulo),14000);
+            for (let i = 0; i < textoArray.length; i++) {
+                setTimeout(() => elemento.innerHTML += textoArray[i], 170 * i);
+            }
+        }, 1500);
+
+        if (repeatCount < maxRepeats) {
+            repeatCount++;
+            setTimeout(() => typeWriter(titulo), 14000);
+        }
     }
+
     const titulo = document.querySelector('.nome');
     typeWriter(titulo);
 }
